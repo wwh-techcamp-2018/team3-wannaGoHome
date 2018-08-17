58219fdcab98004fd2e26a5810c30b3f8c6a74b4
Song Intae <>
2018-08-17 12:53:25 +0900

    BoardSummary.js 변경
    1. drawTeamBoards 내용 변경

--- src/main/resources/static/js/class/BoardSummary.js
+++ src/main/resources/static/js/class/BoardSummary.js
@@ -6,6 +6,8 @@ document.addEventListener("DOMContentLoaded", function(evt) {
 class BoardSummary {
     constructor(boardSummary) {
         this.node = boardSummary;
+        this.recentlyBoardList = this.node.querySelector(".recent-board-list");
+        this.makeTeamContainer = this.node.querySelector(".makeTeam-container");
     }
 
     requestBoardSummary() {
@@ -19,27 +21,29 @@ class BoardSummary {
 
     drawBoardSummary(result) {
         this.drawRecentlyViewBoards(result.recentlyViewBoards);
-        this.drawTeamBoards(result.boardOfTeamDtos);
+        this.drawAllTeamBoards(result.boardOfTeamDtos);
         this.addCreateNewBoardEvent();
     }
 
     drawRecentlyViewBoards(recentlyViewBoards) {
-        const recentlyBoardList = this.node.querySelector(".recent-board-list");
         recentlyViewBoards.forEach((board) => {
-            recentlyBoardList.innerHTML += this.getBoardTemplate(board);
+            this.recentlyBoardList.innerHTML += this.getBoardTemplate(board);
         });
     }
-    drawTeamBoards(teamBoards) {
-        const makeTeamContainer = this.node.querySelector(".makeTeam-container");
-        const template = Handlebars.templates["precompile/team_boards_template"];
+    drawAllTeamBoards(teamBoards) {
         for(const teamBoard of teamBoards) {
-            makeTeamContainer.insertAdjacentHTML("beforebegin", template(teamBoard.team));
-            for(const board of teamBoard.boards) {
-                const createBoard = this.node.querySelector(".board-card.create-board-card");
-                createBoard.insertAdjacentHTML("beforebegin",this.getBoardTemplate(board));
-            }
+            this.drawTeamBoards(teamBoard)
         };
     }
+    drawTeamBoards(teamBoard) {
+        const template = Handlebars.templates["precompile/team_boards_template"];
+        this.makeTeamContainer.insertAdjacentHTML("beforebegin", template(teamBoard.team));
+        for(const board of teamBoard.boards) {
+            const createBoard = this.node.querySelector(".board-card.create-board-card");
+            createBoard.insertAdjacentHTML("beforebegin",this.getBoardTemplate(board));
+        }
+    }
+
     getBoardTemplate(board) {
         const template = Handlebars.templates["precompile/board_template"];
         return template(board);

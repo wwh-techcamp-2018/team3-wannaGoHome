4576a3ac3f7711db4fd77f4a1755189dc8aa0ffe
KimYeon <kimyeon0331@likelion.org>
2018-08-20 16:47:10 +0900

    기늧추가 : getBoundingRect함수 추가

--- src/main/resources/static/js/common/common.js
+++ src/main/resources/static/js/common/common.js
@@ -39,3 +39,8 @@ function hex(x) {
     ("0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f");
     return isNaN(x) ? "00" : hexDigits[(x - x % 16) / 16] + hexDigits[x % 16];
 }
+
+function getBoundingRect(element) {
+    const rect = element.getBoundingClientRect();
+    return rect;
+}

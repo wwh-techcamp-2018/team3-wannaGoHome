761f61e8a7c85752c79dc96554604cafd9f1cd0c
Jae-Hyeong <jhyang123494@gmail.com>
2018-08-21 10:57:02 +0900

    기능 구현: 프런트엔드 채팅 뼈대 구현 완료

---
+++ src/main/java/wannagohome/domain/ChatMessage.java
@@ -0,0 +1,4 @@
+package wannagohome.domain;
+
+public class ChatMessage {
+}
--- src/main/resources/static/js/board/board.js
+++ src/main/resources/static/js/board/board.js
@@ -1,4 +1,21 @@
 document.addEventListener("DOMContentLoaded", function(evt) {
    const board = new Board();
+
+    window.addEventListener("resize", function(evt) {
+        let chattingBarWidth = getBoundingRect($_(".chatting-bar")).width;
+        $_(".board-scroll-container").style.width = document.documentElement.clientWidth - chattingBarWidth + "px";
+
+        let navbarHeight = getBoundingRect($_("header")).height;
+        $_(".chatting-bar").style.height = document.documentElement.clientHeight - navbarHeight + "px";
+        $_(".chatting-bar").style.top = navbarHeight + "px";
+
+        let chattingBarHeight = getBoundingRect($_(".chatting-bar")).height;
+        let chattingBarTitleHeight = getBoundingRect($_(".chatting-bar-title")).height;
+        let chattingInputHolderHeight = getBoundingRect($_(".chatting-input-holder")).height;
+        $_(".chatting-bar-message-holder").style.height = chattingBarHeight - chattingBarTitleHeight - chattingInputHolderHeight - 36 + "px";
+    });
+
+    window.dispatchEvent(new Event("resize"))
+
 });
 

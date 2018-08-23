d6a72f4ef500b0c3b78563d16ad4af9052b99de5
junsulime <vfv0121@gmail.com>
2018-08-23 22:47:14 +0900

    리펙토링: flywaymigration v1 코드 수정

--- src/main/resources/db/migration/V1.01__initial_table_create.sql
+++ src/main/resources/db/migration/V1.01__initial_table_create.sql
@@ -163,30 +163,3 @@ CREATE TABLE user_included_in_team (
  CONSTRAINT FKbdlvs6m98ts2seg0r6ho7fl22 FOREIGN KEY (user_id) REFERENCES user (id),
  CONSTRAINT FKsiqi77b1dg35nvugar2yy7ntc FOREIGN KEY (team_id) REFERENCES team (id)
 ) ENGINE=InnoDB ;
--- $2a$10$yKTRTpg8iaZBSGe.kIQYzuAF0A2nrUQ2YAMwz50mqllpo9fqpk0XO is "password1" encoded by BcryptEncoder
-INSERT INTO user (email, name, password, deleted) VALUES('junsulime@woowahan.com', 'junsulime', '$2a$10$yKTRTpg8iaZBSGe.kIQYzuAF0A2nrUQ2YAMwz50mqllpo9fqpk0XO', false);
-INSERT INTO user (email, name, password, deleted) VALUES('jhyang12345@woowahan.com', 'jhyang', '$2a$10$yKTRTpg8iaZBSGe.kIQYzuAF0A2nrUQ2YAMwz50mqllpo9fqpk0XO', false);
-INSERT INTO user (email, name, password, deleted) VALUES('songintae@woowahan.com', 'songintae', '$2a$10$yKTRTpg8iaZBSGe.kIQYzuAF0A2nrUQ2YAMwz50mqllpo9fqpk0XO', false);
-INSERT INTO user (email, name, password, deleted) VALUES('kimyeon@woowahan.com', 'kimyeon', '$2a$10$yKTRTpg8iaZBSGe.kIQYzuAF0A2nrUQ2YAMwz50mqllpo9fqpk0XO', false);
-INSERT INTO team (profile_image, name, description, deleted) VALUES ('http://urlrul', 'JunsuLime', 'junsulime description', false);
-INSERT INTO team (profile_image, name, description, deleted) VALUES ('http://urlrul', '우아한 형제들', '우아한 형제들 description', false);
-INSERT INTO team (profile_image, name, description, deleted) VALUES ('http://urlrul', 'wannaGoHome', 'wannaGoHome description', false);
-INSERT INTO board(id,color,deleted,title,team_id) VALUES(1,'SLIGHTLY_DESATURATED_CYAN',false,'one board',1);
-INSERT INTO board(id,color,deleted,title,team_id) VALUES(2,'SLIGHTLY_DESATURATED_CYAN',false,'two board',1);
-INSERT INTO board(id,color,deleted,title,team_id) VALUES(3,'VERY_DARK_BLUE',false,'three board',1);
-INSERT INTO board(id,color,deleted,title,team_id) VALUES(4,'DARK_LIME_GREEN',false,'four board',1);
-INSERT INTO recently_view_board(id,board_id,user_id)VALUES(1,1,3);
-INSERT INTO recently_view_board(id,board_id,user_id)VALUES(2,1,3);
-INSERT INTO recently_view_board(id,board_id,user_id)VALUES(3,1,3);
-INSERT INTO recently_view_board(id,board_id,user_id)VALUES(4,1,3);
-INSERT INTO recently_view_board(id,board_id,user_id)VALUES(5,1,2);
-INSERT INTO user_included_in_board(id,permission,board_id,user_id)VALUES(1,'ADMIN',1,3);
-INSERT INTO user_included_in_board(id,permission,board_id,user_id)VALUES(2,'ADMIN',2,3);
-INSERT INTO user_included_in_board(id,permission,board_id,user_id)VALUES(3,'ADMIN',3,3);
-INSERT INTO user_included_in_board(id,permission,board_id,user_id)VALUES(4,'ADMIN',4,3);
-INSERT INTO user_included_in_board(id,permission,board_id,user_id)VALUES(5,'ADMIN',1,2);
-INSERT INTO user_included_in_team(id,permission,team_id,user_id)VALUES(1,'ADMIN',1,3);
-INSERT INTO user_included_in_team(id,permission,team_id,user_id)VALUES(2,'ADMIN',2,3);
-INSERT INTO user_included_in_team(id,permission,team_id,user_id)VALUES(3,'ADMIN',1,1);
-INSERT INTO user_included_in_team(id,permission,team_id,user_id)VALUES(4,'ADMIN',1,4);
-INSERT INTO abstract_activity(activity_type,type,source_id,receiver_id,board_id,registered_date) VALUES('BoardActivity', 'BOARD_CREATE', 3, 3 , 1,'2018-08-23 14:45:47');

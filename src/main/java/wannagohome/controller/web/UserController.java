5a80f876c0b9bfe5f56e8b76dc4d7efef9e6c781
Jae-Hyeong Yang <jhyang12345@naver.com>
2018-08-24 15:12:00 +0900

    수정 사항 : 보드 헤더 스타일 수정 (#76)
    * 수정 사항 : 보드 페이지 헤드바 추가 및 버그 대폭 수정
    
    * 수정 사항 : 채팅 오른쪽 메시지 떠 있는 버그 수정
    
    * 수정 사항 : 채팅창 스크롤 문제 해결중
    
    * 기능 추가 : 커스텀 스크롤바 추가
    
    보드 헤더바 스타일 수정 및 개편
    
    * 수정 사항 : 스크롤 자동으로 옆으로 해주는 버그 수정
    
    * 수정 사항 : 스크롤 버그 다시 수정
    
    * 수정 사항 : HTML 인젝션 및 태스크 이동 관련 버그 수정
    
    * 수정 사항 : 헤더 바 색상 변경

--- src/main/java/wannagohome/controller/web/UserController.java
+++ src/main/java/wannagohome/controller/web/UserController.java
@@ -2,6 +2,7 @@ package wannagohome.controller.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import wannagohome.util.SessionUtil;
 

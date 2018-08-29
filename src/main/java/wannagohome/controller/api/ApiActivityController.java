07180061039de4971815c215bd17f765154c4ad1
KimYeon <kimyeon0331@likelion.org>
2018-08-29 22:49:51 +0900

    기능구현 : swagger ui 정리

--- src/main/java/wannagohome/controller/api/ApiActivityController.java
+++ src/main/java/wannagohome/controller/api/ApiActivityController.java
@@ -1,5 +1,8 @@
 package wannagohome.controller.api;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,11 +18,14 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/activities")
+@Api(value = "Activity", description = "acticity 기록 API")
 public class ApiActivityController {
 
     @Autowired
     private ActivityService activityService;
 
+    @ApiOperation(value = "해당 유저의 activity 불러오기")
+    @ApiImplicitParam(name = "activity", value = "activity Dto 리스트", required = true,  paramType = "json")
     @PostMapping("/fetch")
     public List<ActivityDto> fetch(@LoginUser User user, @RequestBody RequestActivityDto requestActivityDto) {
         return activityService.fetchActivity(user, requestActivityDto.getRegisteredDate());
--- src/main/java/wannagohome/controller/api/ApiUserController.java
+++ src/main/java/wannagohome/controller/api/ApiUserController.java
@@ -1,5 +1,8 @@
 package wannagohome.controller.api;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -19,6 +22,7 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/users")
+@Api(value = "User", description = "User 관리 API")
 public class ApiUserController {
 
     @Autowired
@@ -33,12 +37,16 @@ public class ApiUserController {
     @Autowired
     private TeamInviteService teamInviteService;
 
+    @ApiOperation(value = "회원가입 요청")
+    @ApiImplicitParam(name = "signUp", value = "User", required = true,  paramType = "json")
     @PostMapping("")
     @ResponseStatus(HttpStatus.CREATED)
     public User signUp(@RequestBody @Valid SignUpDto dto) {
         return userService.signUp(dto);
     }
 
+    @ApiOperation(value = "로그인 요청")
+    @ApiImplicitParam(name = "signIn", value = "User", required = true,  paramType = "json")
     @PostMapping("/signin")
     public User signIn(HttpSession session, @RequestBody @Valid SignInDto dto) {
         User user = userService.signIn(dto);
@@ -46,16 +54,23 @@ public class ApiUserController {
         return user;
     }
 
+
+    @ApiOperation(value = "로그아웃 요청")
+    @ApiImplicitParam(name = "logout", required = true,  paramType = "json")
     @PostMapping("/logout")
     public void logout(HttpSession session) {
         SessionUtil.removeUserSession(session);
     }
 
+    @ApiOperation(value = "세션 유저 가져오기")
+    @ApiImplicitParam(name = "getUserDtoInSession", value = "UserDto", required = true,  paramType = "json")
     @GetMapping("/userId")
     public UserDto getUserDtoInSession(HttpSession session) {
         return SessionUtil.getUserSession(session).getUserDto();
     }
 
+    @ApiOperation(value = "mypage 정보 가져오기")
+    @ApiImplicitParam(name = "profile", value = "MyPageDto", required = true,  paramType = "json")
     @GetMapping("/profile")
     public MyPageDto profile(@LoginUser User user) {
         return MyPageDto.valueOf(
@@ -66,22 +81,30 @@ public class ApiUserController {
         );
     }
 
+    @ApiOperation(value = "profile 초기화")
+    @ApiImplicitParam(name = "initializeProfile", value = "UserDto", required = true,  paramType = "json")
     @PostMapping("/profile/init")
     public UserDto initializeProfile(@LoginUser User user) {
         return UserDto.valueOf(userService.initializeProfile(user));
     }
 
+    @ApiOperation(value = "profile 변경하기")
+    @ApiImplicitParam(name = "changeProfile", value = "UserDto", required = true,  paramType = "json")
     @PostMapping("/profile")
     public UserDto changeProfile(@LoginUser User user, @RequestPart MultipartFile file){
         return UserDto.valueOf(userService.changeProfile(user,file));
     }
 
+    @ApiOperation(value = "user 이름 변경하기")
+    @ApiImplicitParam(name = "changeName", value = "UserDto", required = true,  paramType = "json")
     @PutMapping("/profile")
     public UserDto changeName(@LoginUser User user, @Valid @RequestBody UserDto userDto){
         user.setName(userDto.getName());
         return UserDto.valueOf(userService.save(user));
     }
 
+    @ApiOperation(value = "팀 초대하기")
+    @ApiImplicitParam(name = "processTeamInvitation", value = "TeamInvite", required = true,  paramType = "json")
     @PostMapping("/invitation")
     public TeamInvite processTeamInvitation(@LoginUser User user, @RequestBody TeamInvitationDto invitationDto) {
         return userService.processTeamInvitation(user, invitationDto);

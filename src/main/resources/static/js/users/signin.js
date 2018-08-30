cd779d5f4aefcc10827f594b3ac0cf81b267c1b0
junsulime <vfv0121@gmail.com>
2018-08-30 22:17:17 +0900

    버그수정: 로그인 상태 및 로그인 상태 아닌 경우 이상한 일 막기

--- src/main/resources/static/js/users/signin.js
+++ src/main/resources/static/js/users/signin.js
@@ -1,3 +1,4 @@
 document.addEventListener("DOMContentLoaded", function (evt) {
     new SignForm(document.querySelector("form"), "/api/users/signin", "/");
+    window.addEventListener('popstate', (evt) => {window.location.href="/"});
 });
